6147eeb04e20d3cd3f3abe86896367422bcda450
JumbleRuble <tanishasharda7@gmail.com>
2021-02-19 14:50:17 +0530

    started signup

--- foodfighters/app/src/main/java/com/tani/foodfighters/MainActivity.kt
+++ foodfighters/app/src/main/java/com/tani/foodfighters/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.tani.foodfighters
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun start(view: View){
+        var intent = Intent(applicationContext, start::class.java)
+        startActivity(intent)
+    }
+
+    fun signUp(view: View){
+        var intent = Intent(applicationContext, signup::class.java)
+        startActivity(intent)
+    }
+
+
 }
---
+++ foodfighters/app/src/main/java/com/tani/foodfighters/signup.kt
@@ -0,0 +1,35 @@
+package com.tani.foodfighters
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class signup : AppCompatActivity() {
+    var userType = -1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+
+    fun viewDetails(view: View){
+        if(userType==-1){
+            Toast.makeText(applicationContext, "Select an option first", Toast.LENGTH_LONG).show()
+        }
+        else {
+            val intent = Intent(applicationContext, datails::class.java)
+            intent.putExtra("user", userType)
+            startActivity(intent)
+        }
+    }
+
+    fun donor(view: View){
+        userType = 1
+    }
+
+    fun dist(view: View){
+        userType = 2
+    }
+
+}
